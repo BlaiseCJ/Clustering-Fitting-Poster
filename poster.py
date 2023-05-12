aafa961c4f6aa5df0090480b6ebcfb9c5bc63629
BlaiseCJ <97786081+BlaiseCJ@users.noreply.github.com>
2023-05-12 21:03:31 +0100

    Changed Dataset
    I have changed the dataset as I now cluster and fit the dataset about global C)2 emissions from 1960 to 2018

--- poster.py
+++ poster.py
@@ -5,48 +5,135 @@ Created on Thu May 11 00:54:11 2023
 @author: Blaise Ezeokeke
 """
 
+import sklearn.datasets as skdat
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import cluster_tools as ct
+import matplotlib.pyplot as plt
+import sklearn.cluster as cluster
+import sklearn.metrics as skmet
 
-def read_csv_file(file_path):
+
+emission_data = pd.read_csv('CO2_Emissions_1960-2018.csv')
+print(emission_data)
+
+print(emission_data.describe())
+
+emission_df = emission_data[["1971", "1981", "1991", "2001", "2016"]]
+print(emission_df.describe())
+
+emission_df = emission_df.drop(["1971", "1981"], axis=1)
+print(emission_df.describe())
+
+# Define the row and column labels
+index = ['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+columns = ['1991', '2001', '2016']
+
+# Create a DataFrame from the data
+df_heatmap = pd.DataFrame(emission_df, index=index, columns=columns)
+
+corr = df_heatmap.corr()
+
+# Define custom colors for the boxes
+colors = ['#FF0000', '#FFA500', '#0000FF', '#ADD8E6']  # Red, Orange, Blue, Light Blue
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f', cbar=True,
+            linewidths=0.5, linecolor='black', square=True)
+plt.title('Correlation Heatmap with Inner Blocks')
+plt.show()
+
+pd.plotting.scatter_matrix(emission_df, figsize=(12, 12), s=5, alpha=0.8)
+
+plt.show()
+
+# Select the columns for clustering
+df_cluster = emission_df[["1991", "2016"]]
+
+df_cluster = df_cluster.dropna() 
+df_cluster = df_cluster.reset_index()
+print(df_cluster.iloc[0:15])
+
+# remove the old index from column index before clustering
+df_cluster = df_cluster.drop("index", axis=1)
+print(df_cluster.iloc[0:15])
+
+# Create and store minimum and maximum, and normalise
+norm_df, min_df, max_df = ct.scaler(df_cluster)
+
+print()
+print("n  score")
+
+# loop over number of clusters
+for scluster in range(2, 10):
     
+    # set up the clusterer with the number of expected clusters
+    kmeans = cluster.KMeans(n_clusters=scluster)
+
+    # Fit the data, results are stored in the kmeans object
+    kmeans.fit(norm_df)     # fit done on x,y pairs
+
+    label = kmeans.labels_
     
-    """
-    Returns a Pandas DataFrame, after reading CSV files with Pandas.
+    # extract the estimated cluster centres
+    centre = kmeans.cluster_centers_
 
-    Args:
-        file_path (str): The CSV file file-path.
+    # calculate the silhoutte score
+    print(scluster, skmet.silhouette_score(df_cluster, label))
+    
+    scluster = 7 
 
-    Returns:
-        pandas.DataFrame: The CSV file data returned as a pandas DataFrame.
-    """
-    # Read the CSV file into a Pandas DataFrame
-    data_df = pd.read_csv(file_path)
-
-    # Return the DataFrame
-    return data_df
+# Setting up the clusterer
+kmeans = cluster.KMeans(n_clusters=scluster)
 
 
-# Read the Global CO2 emission data
-co2_df = read_csv_file('co2_emissions_kt_by_country.csv')
+kmeans.fit(norm_df)    
 
-# Preview the head of the Global CO2 emissions data
-print("Global CO2 Emission Data:")
-print(co2_df.head())
+label = kmeans.labels_
+    
+# Bringing the cluster centres
+centre = kmeans.cluster_centers_
 
-# Display Global CO2 emission summary statistics
-print("Summary Statistics for Global CO2 Emission Data:")
-print(co2_df.describe())
+centre = np.array(centre)
+xcentre = centre[:, 0]
+ycentre = centre[:, 1]
 
 
+# Plot the clusters
+plt.figure(figsize=(8.0, 8.0))
 
-# Read the Global population data
-pop_df = read_csv_file('world_population.csv')
+cluster_map = plt.cm.get_cmap('tab10')
+plt.scatter(norm_df["1991"], norm_df["2016"], 10, label, marker="o", cmap=cluster_map)
+plt.scatter(xcentre, ycentre, 45, "k", marker="d")
+plt.xlabel("Emissions(1991)")
+plt.ylabel("Emissions(2016)")
+plt.show()
 
-# Preview the head of the Global population data
-print("Global Population Data:")
-print(pop_df.head())
+scluster = 7 
 
-# Display Global Population data summary statistics
-print("Summary Statistics for Global Population Data:")
-print(pop_df.describe())
+# Setting up the clusterer
+kmeans = cluster.KMeans(n_clusters=scluster)
 
+
+kmeans.fit(norm_df)    
+
+label = kmeans.labels_
+    
+# Bringing the cluster centres
+centre = kmeans.cluster_centers_
+
+centre = np.array(centre)
+xcentre = centre[:, 0]
+ycentre = centre[:, 1]
+
+
+# Plot the clusters
+plt.figure(figsize=(8.0, 8.0))
+
+cluster_map = plt.cm.get_cmap('tab10')
+plt.scatter(norm_df["1991"], norm_df["2016"], 10, label, marker="o", cmap=cluster_map)
+plt.scatter(xcentre, ycentre, 45, "k", marker="d")
+plt.xlabel("Emissions(1991)")
+plt.ylabel("Emissions(2016)")
+plt.show()
